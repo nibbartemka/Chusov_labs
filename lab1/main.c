fe27b9d4bb2c1eb72993993ed5c5fc4a4a80e973
root <root@CT115.ru>
2026-02-03 11:50:44 +0000

    FEAT(lab1): add files with measures

--- lab1/main.c
+++ lab1/main.c
@@ -1,7 +1,9 @@
-#include <string.h>
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 #include <omp.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
 
 #define N (1u << 27)
 
@@ -13,6 +15,84 @@
         printf("%-35s %8.4f с | результат: %.6f \n", \
             func_name, t1 - t0, res); \
     } while(0)
+
+
+#define SPEEDTEST(func, n, exp) \
+    speedtest_avg_openmp(#func, func, n, exp)
+
+void speedtest_avg_openmp(
+    const char* name,
+    double (*func)(const double*, unsigned int),
+    size_t n,
+    size_t exp_count
+) {
+    printf("======= SPEEDTEST OPEN MP AVG ==========\n");
+
+    char filename[256];
+    snprintf(filename, sizeof(filename), "%s.csv", name);
+
+    FILE* output = fopen(filename, "w");
+    if (!output) {
+        printf("Error while opening file\n");
+        return;
+    }
+
+    fprintf(output, "T,Time,Avg,Acceleration\n");
+
+    double time_sum_1 = 0.0;
+    int max_threads = sysconf(_SC_NPROCESSORS_ONLN);
+
+    for (int thread_num = 1; thread_num <= max_threads; thread_num++) {
+
+        omp_set_num_threads(thread_num);
+
+        double time_sum = 0.0;
+        double t0 = omp_get_wtime();
+
+        for (size_t exp = 0; exp < exp_count; exp++) {
+
+            double* p = (double*)malloc(n * sizeof(double));
+            if (!p) {
+                printf("Memory allocation error\n");
+                fclose(output);
+                return;
+            }
+
+            for (size_t i = 0; i < n; i++) {
+                p[i] = (double)i;
+            }
+
+            double t1 = omp_get_wtime();
+            double result = func(p, n);
+            double t2 = omp_get_wtime();
+
+            time_sum += (t2 - t1) * 1000.0;
+
+            free(p);
+        }
+
+        double total_time = (omp_get_wtime() - t0) * 1000.0;
+
+        if (thread_num == 1) {
+            time_sum_1 = time_sum;
+        }
+
+        double avg = time_sum / exp_count;
+        double acceleration = (time_sum_1 / exp_count) / avg;
+
+        printf(
+            "AVG: T = %d\t| total experiment time: %.2f ms\t| avg time = %.2f ms\t| acceleration = %.2f\n",
+            thread_num, total_time, avg, acceleration
+        );
+
+        fprintf(
+            output, "%d,%.2f,%.2f,%.2f\n",
+            thread_num, total_time, avg, acceleration
+        );
+    }
+
+    fclose(output);
+}
 
 
 double avg(const double* v, size_t n) {
@@ -148,19 +228,24 @@ double avg_omp_with_cache_optimizing(const double* v, size_t n) {
 }
 
 int main() {
-    double* p = (double*)malloc(N * sizeof(double));
+    // double* p = (double*)malloc(N * sizeof(double));
 
-    for (size_t i = 0; i < N; i++) {
-        p[i] = i;
-    }
+    // for (size_t i = 0; i < N; i++) {
+    //     p[i] = i;
+    // }
 
-    MEASURE("Последовательная (avg)", avg(p, N));
-    MEASURE("OpenMP reduction", avg_omp_reduction(p, N));
-    MEASURE("OpenMP basic parallelism", avg_omp_parallel(p, N));
-    MEASURE("OpenMP optimized parallelism", avg_omp_parallel_optimized(p, N));
-    MEASURE("OpenMP + cache optimizing", avg_omp_with_cache_optimizing(p, N));
+    // MEASURE("Последовательная (avg)", avg(p, N));
+    // MEASURE("OpenMP reduction", avg_omp_reduction(p, N));
+    // MEASURE("OpenMP basic parallelism", avg_omp_parallel(p, N));
+    // MEASURE("OpenMP optimized parallelism", avg_omp_parallel_optimized(p, N));
+    // MEASURE("OpenMP + cache optimizing", avg_omp_with_cache_optimizing(p, N));
 
-    free(p);
+    // free(p);
+
+    SPEEDTEST(avg_omp_reduction, N, 5);
+    SPEEDTEST(avg_omp_parallel, N, 5);
+    SPEEDTEST(avg_omp_parallel_optimized, N, 5);
+    SPEEDTEST(avg_omp_with_cache_optimizing, N, 5);
 
     return 0;
 }
